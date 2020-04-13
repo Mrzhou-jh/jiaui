5d343c5275757ba1ad33582bf55c1599af052b16
MrZhou-jh <211535176@qq.com>
2020-04-13 17:29:51 +0800

    g-button ui

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <j-button>按钮</j-button>
+    <j-button svgicon="shezhi" iconPosition="right">设置</j-button>
+    <j-button svgicon="zan">点赞</j-button>
+    <j-button svgicon="jiazai" iconPosition="right" :loading="true">加载</j-button>
+    <j-button svgicon="shangchuan" iconPosition="right" @click="loading = !loading" :loading="loading">加载</j-button>
+    <j-button svgicon="xiazai" :loading="loading2" @click="loading2 = !loading2">下载</j-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return { loading: false, loading2: false };
+  }
+};
+</script>
+
+<style></style>
